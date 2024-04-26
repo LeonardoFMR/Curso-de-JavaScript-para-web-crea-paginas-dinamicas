cea652b4f7f1d1384c290b54d35dc685a1406ce6
Leonardo <leonardfmr@gmail.com>
2024-04-25 20:27:26 -0600

    Last changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Función que busca los elementos de audio
+//Función que busca los elementos de audio y reproduce el sonido asociado al elemento de audio con el ID proporcionado
 function playSonido (idElementoAudio){ // Se inicia la funcion dando como parámetro idElementoAudio.
     document.querySelector(idElementoAudio).play(); // Se accede mediante el DOM mediante el método que le sucede. Se invoca a la función misma dentro si. Se busca el idElementoAudio.
 } // Cierre de la función
@@ -13,8 +13,8 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++ ){ // Se inici
     tecla.onclick = function(){ // Empleando una función anónima se establece un evento de click en el elemento tecla para que se inicie la función
         playSonido(idAudio); // Se llama a la función playSonido dándole como párametro la cadena de texto que se almacena en la constante idAudio
     };
-    tecla.onkeydown = function(evento){ // Al elemento se agrega un escuchador de eventos cuando la tecla este presionada, lo que inicializa la función anónima. También: "evento" en este contexto es un parámetro que representa el evento de teclado que está siendo manejado
-       console.log(evento.code === 'Space' || evento.code === 'Enter')
+    tecla.onkeydown = function(evento){ // Al elemento se agrega un escuchador de eventos cuando se presiona una tecla del teclado, lo que inicializa la función anónima. También: "evento" en este contexto es un parámetro que representa el evento de teclado que está siendo manejado.
+     
         if (evento.code === 'Space'|| evento.code === 'Enter' ){ // Se estabe lecque la condición que se debe cumpir es que "evento" mediante la propiedad "code" debe ser igual a "Space" o "Enter" mediante el operador boleano OR ||.
             tecla.classList.add('activa'); // Se le añade una clase al elmeneto tecla. Buscando en classList. Se usa el método add para añadir una clase específica al elemento en est caso tecla.
         }
@@ -25,11 +25,3 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++ ){ // Se inici
     }
 }
 
-
-En primer lugar, debes crear una referencia para obtener la lista de todas las teclas de AluraFone y otra para capturar el input "Ingresa tu teléfono". En este caso, las referencias const listaDeTeclas e const inputTel, respectivamente.
-
-El segundo paso es crear un bucle for que recorrerá la lista de teclas. Por lo tanto, la condición del for es indice < listaDeTeclas.length, es decir, mientras el índice sea menor que el tamaño de listaDeTeclas, se ejecutará un comando.
-
-Dentro del for, debes crear una referencia constante const tecla para que, en cada iteración, reciba el valor correspondiente de la tecla listaDeTeclas[indice].
-
-Después de esto, agrega una función anónima al onclick que recibirá el valor del campo "Ingresa tu teléfono" inputTel.value y le asignará la suma entre el valor actual del campo "Ingresa tu teléfono" y el valor de la tecla clicada tecla.value, quedando así inputTel.value = inputTel.value + tecla.value.
